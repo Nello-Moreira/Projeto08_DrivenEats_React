4e5868133d1f1f9d740cc146e4dffe830f2d1f96
Nello-Moreira <g.moreira1@outlook.com>
2021-08-29 20:30:33 -0300

    bug fixed: section id

--- src/components/sectionComponents/Section.js
+++ src/components/sectionComponents/Section.js
@@ -3,31 +3,31 @@ import SectionTitle from './SectionTitle'
 import Option from '../optionComponents/Option'
 
 export default function Section({ sectionInfos, activeChilds, parentRecordChanger }) {
-    const { sectionId, title, options } = sectionInfos;
+    const { id, title, options } = sectionInfos;
 
     const childActivationHandler = (child) => {
         const isNewChild = activeChilds.filter(element => element.id === child.id).length === 0;
 
         if (isNewChild) {
             activeChilds.push(child);
-            parentRecordChanger(sectionId, activeChilds);
+            parentRecordChanger(id, activeChilds);
             return;
         }
         if (child.itemQuantity === 0) {
             activeChilds = activeChilds.filter(element => element.id !== child.id);
-            parentRecordChanger(sectionId, activeChilds);
+            parentRecordChanger(id, activeChilds);
             return;
         }
         activeChilds.forEach((element, index, thisArray) => {
             if (element.id === child.id && element.itemQuantity !== child.itemQuantity) {
                 thisArray[index] = child;
-                parentRecordChanger(sectionId, activeChilds);
+                parentRecordChanger(id, activeChilds);
             }
         })
     };
 
     return (
-        <section id={sectionId} className="section">
+        <section id={id} className="section">
             <SectionTitle title={title} />
             <ul>
                 {options.map((option, index) => (
