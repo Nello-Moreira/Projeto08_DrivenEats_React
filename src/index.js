07a59714558b1fdd6b53c2c356bf890bc51d6e94
Matheus Galdino <matheus@respondeai.com.br>
2021-08-31 12:55:51 -0300

    preparando o review

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./mainCSS/reset.css";
-import "./mainCSS/styles.css";
-import "./mainCSS/buttons.css";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
